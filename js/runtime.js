a0575b479c42a6b5f2f889cb25abf0e7c6e3d78a
Lwt235 <m15955295202@163.com>
2023-08-26 18:28:59 +0800

    Site updated: 2023-08-26 18:28:55

--- js/runtime.js
+++ js/runtime.js
@@ -1,5 +1,5 @@
 setInterval(() => {
-    let create_time = Math.round(new Date('2023-08-01 00:00:00').getTime() / 1000); //在此行修改建站时间
+    let create_time = Math.round(new Date('2023-08-20 00:00:00').getTime() / 1000); //在此行修改建站时间
     let timestamp = Math.round((new Date().getTime()) / 1000);
     let second = timestamp - create_time;
     let time = new Array(0, 0, 0, 0, 0);

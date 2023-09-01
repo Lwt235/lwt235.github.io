e6d66cbd22eb58dff4baa4d97b1b127a383e424e
Lwt235 <m15955295202@163.com>
2023-09-01 17:39:03 +0800

    Site updated: 2023-09-01 17:39:03

--- js/title.js
+++ js/title.js
@@ -8,7 +8,7 @@ document.addEventListener('visibilitychange', function () {
     clearTimeout(titleTime);
   } else {
     //返回当前页面时标签显示内容
-    document.title = '🐖抓到你啦～';
+    document.title = '抓到你啦～';
     //两秒后变回正常标题
     titleTime = setTimeout(function () {
       document.title = OriginTitile;
